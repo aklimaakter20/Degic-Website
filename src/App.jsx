f9883871d4b6918bfc95478642957bc2a09d8ba1
aklimaakter20 <aklimaakter20@gmail.com>
2025-01-27 18:21:46 +0600

    material ui reinstalled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 
